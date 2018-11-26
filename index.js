83f7a53c2ddb3720b8592632f035632ed3c3d9e1
Ayana Bando <ayanaflax@yahoo.com>
2018-11-26 15:52:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function destructivelyRemoveFirstDriver(){
   drivers.shift();
 }
 function appendDriver(name){
-  name = [...drivers.slice(-1);
+  const name = [...drivers.slice(0,-1), "Broom"];
 }
